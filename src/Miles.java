464598f938938b545c9d001aa35c724da45eec30
stodoriko <s.todorikos@gmail.com>
2021-07-28 00:05:31 +0300

    added Miles

---
+++ src/Miles.java
@@ -0,0 +1,10 @@
+public class Miles {
+    public static void main(String[] args) {
+        int ticket_price = 75;
+
+        int bonus = ticket_price / 20;
+
+        System.out.println("Количество бонусных миль: " + bonus);
+
+    }
+}
